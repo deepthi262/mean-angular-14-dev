9e2e9605e859599a8c0d1bc22146aad65bf9004b
Deepthi <deepthi262@gmail.com>
2024-07-03 18:14:07 +0000

    updated scripts

--- mean-dev/mean-stack-example/01-mongo-db-docker-compose/03-mongodb-shell-connect.sh
+++ mean-dev/mean-stack-example/01-mongo-db-docker-compose/03-mongodb-shell-connect.sh
@@ -1,3 +1,4 @@
 #mongosh "mongodb://localhost:27017"
-mongosh "mongodb://mongo:mongo@localhost:27017/"
+#mongosh "mongodb://mongo:mongo@localhost:27017/"
 #mongosh "mongodb://mongo:mongo@localhost:27017/bezkoder_db"
+mongosh "mongodb://mongo:mongo@0.0.0.0:27017/bezkoder_db"
--- mean-dev/mean-stack-example/01-mongo-db-docker-compose/mongo-init/init-bezkoder_db.mongodb.js
+++ mean-dev/mean-stack-example/01-mongo-db-docker-compose/mongo-init/init-bezkoder_db.mongodb.js
@@ -1,5 +1,13 @@
-conn = new Mongo();
-db = conn.getDB("bezkoder_db");
+//conn = new Mongo();
+//db = conn.getDB("bezkoder_db");
+use('bezkoder_db');
+db.createUser(
+  {
+    user: "mongo",
+    pwd: "mongo",
+    roles: [ { role: "readWrite", db: "bezkoder_db" }]
+  }
+)
 db.createCollection('tutorials');
 db.tutorials.insertOne({
     title: "Js Tut#",
@@ -8,6 +16,7 @@ db.tutorials.insertOne({
     createdAt: Date(),
     updatedAt: Date()
   })
+db.tutorials.find()
 db.myCollectionName.createIndex({ "address.zip": 1 }, { unique: false });
 db.myCollectionName.insert({ "address": { "city": "Paris", "zip": "123" }, "name": "Mike", "phone": "1234" });
 db.myCollectionName.insert({ "address": { "city": "Marsel", "zip": "321" }, "name": "Helga", "phone": "4321" });
